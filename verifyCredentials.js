0d9251edebd0a936b80eb01e2d35f9196cd9e5b3
Leonid Kuzmin <zuker@users.noreply.github.com>
2018-02-28 12:12:42 +0200

    platform#151 added credentials (#7)
    * platform-151 Add credentials to webhook component
    
    * platform-151 added verify creds
    
    * platform-151 removed unused fields
    
    * platform-151 restored payload view
    
    * platform-151 smallfix
    
    * platform-151 review fixes
    
    * platform-151 review fixes

---
+++ verifyCredentials.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = verify;
+
+/**
+ * Executes the verification logic by checking that fields are not empty using the provided apiKey.
+ *
+ * @param credentials object to retrieve apiKey from
+ *
+ * @returns Promise which resolves true
+ */
+
+const authTypes = {
+    BASIC: 'BASIC',
+};
+
+function verify(credentials) {
+    // access the value of the auth field defined in credentials section of component.json
+    const { type, basic = {} } = credentials.auth;
+
+    if (type === authTypes.BASIC) {
+        if (!basic.username) {
+            console.log('Error: Username is required for basic auth');
+            throw new Error('Username is required for basic auth');
+        }
+
+        if (!basic.password) {
+            console.log('Error: Password is required for basic auth');
+            throw new Error('Password is required for basic auth');
+        }
+    }
+
+    return Promise.resolve(true);
+}
