75b204e5830863e76c744adec51eb38d68c67d7b
Arsenii Zhdanov <arsenzhd@gmail.com>
2019-07-26 17:20:23 +0300

    Revert "Webhook should include addtional information in the call #12 (#14)"
    This reverts commit 2313d23b2330cdc23a994e182f95479c2e3dd4a1.

--- send.js
+++ send.js
@@ -1,6 +1,5 @@
-'use strict';
-const request = require('./request.js');
-const DEFAULT_METHOD = 'POST';
+var request = require('./request.js');
+var DEFAULT_METHOD = 'POST';
 
 exports.process = function(msg, conf) {
     request.putOrPost.call(this, conf.method || DEFAULT_METHOD, msg, conf);
